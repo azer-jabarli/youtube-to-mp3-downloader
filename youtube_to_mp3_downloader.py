d23283db97afe4d27078b3f02bdfae43380688a0
Azer Jabarli <azer.cabbar.158@gmail.com>
2021-08-18 17:26:11 +0400

    Added URL validator

--- youtube_to_mp3_downloader.py
+++ youtube_to_mp3_downloader.py
@@ -3,8 +3,15 @@
 
 from pytube import YouTube # Essential library for working with YouTube
 import os # Needed to save the downloaded file
+import validators # Needed to check if the link is valid
 
 video_link = YouTube(input("Enter video URL: ")) # Get video URL
+
+while True: # Check for the validity of the URL
+	if not validators.url(video_link):
+		video_link = YouTube(input("Enter video URL: "))
+	else:
+		break		
 
 audio = video_link.streams.filter(only_audio = True).first() # Extract the audio from the video
 
